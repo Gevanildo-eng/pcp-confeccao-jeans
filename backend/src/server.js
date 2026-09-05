37bbfffbb62b3cf951d98d7a1fa077a9f56be89d
Geanildo <gevanildounplt@gmail.com>
2026-09-05 16:45:35 -0300

    configura coneao com postgreSQL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const pool = require('./config/database');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +13,22 @@ app.get('/', (request, response) => {
     message: 'API do PCP Confecção funcionando!',
   });
 });
+app.get('/database', async (request, response) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
 
+    response.json({
+      message: 'Conexão com o PostgreSQL realizada com sucesso!',
+      databaseTime: result.rows[0].now,
+    });
+  } catch (error) {
+    console.error('Erro ao conectar com o PostgreSQL:', error.message);
+
+    response.status(500).json({
+      message: 'Erro ao conectar com o PostgreSQL.',
+    });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Servidor do PCP funcionando na porta ${PORT}`);
 });
